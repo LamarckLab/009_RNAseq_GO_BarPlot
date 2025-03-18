f5da060a33802e510b5fd3dba7d2a2d10e7a7ba3
L. Mingkai <704021302@qq.com>
2025-03-18 11:35:08 +0800

    Update GO_Bar_Plot.R

--- GO_Bar_Plot.R
+++ GO_Bar_Plot.R
@@ -21,19 +21,12 @@ go_enrich_df <- data.frame(
   )
 )
 
-# 简化GO描述
-for (i in 1:nrow(go_enrich_df)) {
-  description_splite <- strsplit(go_enrich_df$Description[i], split = " ")
-  description_collapse <- paste(description_splite[[1]][1:5], collapse = " ")
-  go_enrich_df$Description[i] <- description_collapse
-}
-go_enrich_df$Description <- gsub(pattern = "NA", "", go_enrich_df$Description)
-
-# 绘制柱状图
-go_enrich_df$type_order <- factor(rev(as.integer(rownames(go_enrich_df))), labels = rev(go_enrich_df$Description))
+# 设置颜色
 COLS <- c("#66C3A5", "#8DA1CB", "#FD8D62")
 
-# 横向柱状图
+# 绘制横向柱状图
+go_enrich_df$type_order <- factor(rev(as.integer(rownames(go_enrich_df))), labels = rev(go_enrich_df$Description))
+
 ggplot(data = go_enrich_df, aes(x = type_order, y = GeneNumber, fill = type)) +
   geom_bar(stat = "identity", width = 0.8) +
   scale_fill_manual(values = COLS) +
@@ -43,14 +36,14 @@ ggplot(data = go_enrich_df, aes(x = type_order, y = GeneNumber, fill = type)) +
   labs(title = "The Most Enriched GO Terms") +
   theme_bw()
 
-###竖着的柱状图 
-go_enrich_df$type_order=factor(rev(as.integer(rownames(go_enrich_df))),labels=rev(go_enrich_df$Description))
-COLS <- c("#66C3A5", "#8DA1CB", "#FD8D62")
-ggplot(data=go_enrich_df, aes(x=type_order,y=GeneNumber, fill=type)) + 
-  geom_bar(stat="identity", width=0.8) + 
+# 绘制竖向柱状图
+go_enrich_df$type_order <- factor(rev(as.integer(rownames(go_enrich_df))), labels = rev(go_enrich_df$Description))
+
+ggplot(data = go_enrich_df, aes(x = type_order, y = GeneNumber, fill = type)) + 
+  geom_bar(stat = "identity", width = 0.8) + 
   scale_fill_manual(values = COLS) + 
   theme_bw() + 
   xlab("GO term") + 
   ylab("Num of Genes") + 
-  labs(title = "The Most Enriched GO Terms")+ 
-  theme(axis.text.x=element_text(face = "bold", color="gray50",angle = 70,vjust = 1, hjust = 1 ))#angle是坐标轴字体倾斜的角度，可以自己设置
+  labs(title = "The Most Enriched GO Terms") + 
+  theme(axis.text.x = element_text(face = "bold", color = "gray50", angle = 70, vjust = 1, hjust = 1))
